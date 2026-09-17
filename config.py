66d9b79ea9479c0cf57d5f51245e47d60aba6d4f
Harishdeloai <harish@deloai.com>
2026-09-17 18:02:13 +0530

    FixS

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/employee_training")
 MONGO_DB = os.getenv("MONGO_DB", "employee_training")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "92dfe91887a9b4b69be14a1ef17582137e092623ab00c3bf60cadcbbf0d9907c")
-FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN", "https://employeetrainingmanagement-1.onrender.com")
+FRONTEND_ORIGIN = os.getenv("FRONTEND_ORIGIN", "https://employeetrainingmanagement-1.onrender.com/api")
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=8)
 
 # The first HR account is created only if these values are supplied.
